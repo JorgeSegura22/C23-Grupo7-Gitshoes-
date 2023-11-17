f4e4a0f5fcc0b3b61579e6973a905d2032e889b8
JorgeSegura22 <ramiropipi1815@gmail.com>
2023-11-17 10:20:52 -0300

    guardando cambios de views y app.js

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const port = 3030;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")))
-app.get("/", (req, res) =>
+app.get("/register", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/views/home.html"))
 );
 
 app.listen(port, () => {
