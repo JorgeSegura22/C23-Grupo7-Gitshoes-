3cebabe6c433e340b9ae67abeca43f3155f41df6
JorgeSegura22 <ramiropipi1815@gmail.com>
2023-11-20 23:59:13 -0300

    agregando ruta de login

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname+"/viwes/register.html"))
 })
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname+"/views/login.html"))
+})
 app.listen(port, () => {
   console.log(`Servidor arrancado en http://localhost:${port}/`);
 });
