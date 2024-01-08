6ae1f8aa2a232e67bdd7e55170438497ac000ba9
wrs0809 <145084286+wrs0809@users.noreply.github.com>
2024-01-07 23:27:01 -0300

    formulario para cargar productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer');
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,path.join(__dirname,"../","../public/images/products"))
+},
+    filename:(req, file, cb) => {
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+}
+})
+const upload = multer({ storage })
 
 const productsController = require("../controllers/productsController")
 
+// Crear Producto
 router.get('/cargaDeProducto',productsController.carga);
+router.post('/cargaDeProducto', upload.array("imagenes"), productsController.almacenar); 
+
 
 
 router.get('/cart',productsController.carrito);
